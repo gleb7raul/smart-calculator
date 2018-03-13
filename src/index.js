09cd53323c16e80d3996250a60fdfcba1d53226d
gleb7raul <glebimptab@gmail.com>
2018-03-13 17:48:38 +0200

    TaskDone

--- src/index.js
+++ src/index.js
@@ -1,27 +1,68 @@
 class SmartCalculator {
   constructor(initialValue) {
-    // your implementation
+      this.expr = '0';
+      if (initialValue) {
+          var op = ' + ' + initialValue;
+          this.expr += op;
+      }
+  }
+
+  valueOf() {
+      var temp = eval(this.expr);
+      return temp;
   }
 
   add(number) {
-    // your implementation
+      var op = ' + ' + number;
+      this.expr += op;
+      return this;
   }
-  
+
   subtract(number) {
-    // your implementation
+      var op = ' - ' + number;
+      this.expr += op;
+      return this;
   }
 
   multiply(number) {
-    // your implementation
+      var op = ' * ' + number;
+      this.expr += op;
+      return this;
   }
 
   devide(number) {
-    // your implementation
+      var op = ' / ' + number;
+      this.expr += op;
+      return this;
   }
-
+  
   pow(number) {
-    // your implementation
+      var op;
+      var arr = this.expr.split(' ');
+      var lastNum = arr[arr.length - 1];
+      if (lastNum[0] === 'M') {
+          arr[arr.length - 1] = this.powFormat(lastNum, number);
+          this.expr = arr.join(' ');
+          return this;
+      }
+
+      op = 'Math.pow(' + lastNum + ',' + number + ')';
+      arr[arr.length - 1] = op;
+      this.expr = arr.join(' ');
+      return this;
   }
+  
+  powFormat(txt, n) {
+      var arr = txt.split(',');
+      var leftSide = arr.slice(0, arr.length - 1).join(',');
+      var lastNum = arr[arr.length - 1].split(')')[0];
+      lastNum = 'Math.pow(' + lastNum + ',' + n;
+      var br = ')';
+      for (var i in txt)
+          if (txt[i] === 'M')
+              br += ')';
+      return [leftSide, lastNum].join(',') + br;
+}
 }
 
 module.exports = SmartCalculator;
